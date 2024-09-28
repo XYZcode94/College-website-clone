55b4f49c2e5a109532ccc2e41f67c3bfe3cf2f37
Prem Prakash <107941698+XYZcode94@users.noreply.github.com>
2024-09-28 20:05:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu = document.getElementsByClassName("imp-link");
+let burger = document.getElementsByClassName("min01");
+
+
+let hamburger = document.getElementsByClassName('hambuger');
+
+
+function show() {
+    
+    menu.classList.toggle('open');
+}
